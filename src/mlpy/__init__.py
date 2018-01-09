27e954889d2772525d0b5c1ce63b3476dab584d4
Andrew <awood7@nd.edu>
2018-01-08 20:01:00 -0500

    dtree works?

--- src/mlpy/__init__.py
+++ src/mlpy/__init__.py
@@ -12,9 +12,10 @@ del _cd_
 # PYTHON PROJECT IMPORTS
 import activation_functions  # noqa: E402
 import bayesian  # noqa: E402
+import data  # noqa: E402
 import data_partitioning  # noqa: E402
-import features  # noqa: E402
 import nets as nets  # noqa: E402
 import processes  # noqa: E402
 import qlearn  # noqa: E402
+import trees  # noqa: E402
 
